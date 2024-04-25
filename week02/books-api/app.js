ace43e2333109017c6382a02b9addbde96f633ac
brazeenNP <s10255790@connect.np.edu.sg>
2024-04-25 11:26:28 +0800

    finished

--- week02/books-api/app.js
+++ week02/books-api/app.js
@@ -30,5 +30,50 @@ let books = [
 
  //get a singular book
  app.get('/books/:id', (req, res) => {
+    const foundid = Number(req.params.id); //parse the ID u want to find
+    const foundbook = books.filter((book) => book.id === foundid); //filter to find the ID in books array
     
- })
+    //if foundbook is not null
+    if (foundbook) {
+        res.json(foundbook); //load the book into the website page
+    }
+    else {
+        res.status(404).send('Book not found'); //send error code and message if not found
+    }
+})
+
+//update a book
+app.put('/books/:id', (req, res) => {
+    const foundid = Number(req.params.id); //parse the ID u want to find
+    const updatedbook = req.body;
+    const foundbookid = books.findIndex((book) => book.id === foundid); //filter to find the ID in books array
+
+    //if book is found, change its body
+    if (foundbookid !== -1) {
+        updatedbook.id = foundid; //updated book has the id of the book it wants to change
+        books[foundbookid] = updatedbook; //replace the book in the books array
+    }
+    else {
+        res.status(404).send('Book not found'); //send error code and message if not found
+    }
+})
+
+//delete a book
+app.delete('/books/:id', (req, res) => {
+    const foundid = Number(req.params.id); //parse the ID u want to find
+    const foundbookid = books.findIndex((book) => book.id === foundid); //filter to find the ID in books array
+
+    //if book found, delete from books array
+    if (foundbookid !== -1) {
+        books.splice(foundbookid, 1); //delete foundbookid index item, deletecount of 1 (only that item)
+        res.status(204).send(); //send 204 status (no content)
+    }
+    else {
+        res.status(404).send('Book not found'); //send error code and message if not found
+    }
+})
+
+//start a localhost server
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+})
