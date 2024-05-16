0af79f10fa0326b10dc4d87d3ae00a67e048b107
brazeenNP <s10255790@connect.np.edu.sg>
2024-05-16 23:24:39 +0800

    practical 5 done

--- week04/books-api-mvc-db/app.js
+++ week04/books-api-mvc-db/app.js
@@ -7,10 +7,11 @@ const validateBook = require("./middlewares/validateBook")
 
 const app = express()
 const port = process.env.PORT || 3000;
-
+const staticMiddleware = express.static("public"); // Path to the public folder
 // Include body-parser middleware to handle JSON data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); // For form data handling   
+app.use(bodyParser.urlencoded({ extended: true })); // For form data handling
+app.use(staticMiddleware)   
 
 app.get("/books", booksController.getAllBooks)
 app.get("/books/:id", booksController.getBookById)
